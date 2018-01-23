b34a0cb7ef82c8b325d7f6473896254e2c40c06b
zy774411963 <381070172@qq.com>
2018-01-23 14:48:30 +0800

    this is javaGit project

---
+++ javaGit/src/javaGit/TestGit.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2018 by FREEMYPAL. Tianhua Information Technology Park (South) Building 4, 4th Floor, 
+ * Longtian Road 195,Shanghai, P.R. China，200235. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of FREEMYPAL
+ * ("Confidential Information"). You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with FREEMYPAL.
+ */
+package javaGit;
+
+/**
+ * @Description: (这里用一句话描述这个类的作用)  
+ * @author yinjia
+ * @version 1.0 2018年1月23日
+ */
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
